0a202eced913a80f6ab7f1b8e909a727b72e08a2
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2023-03-24 16:32:14 +0100

    Changing Api key

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "a8abc05c136ad31a0eb3d9ef118a4a0c";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
